c60297cc2162b29d9f5d002faad808475fe7eb7d
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-03-01 15:35:23 +0100

    scaffolded identity to project

---
+++ LearningAspNetCore_InternetShop/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using LearningAspNetCore_InternetShop.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(LearningAspNetCore_InternetShop.Areas.Identity.IdentityHostingStartup))]
+namespace LearningAspNetCore_InternetShop.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
