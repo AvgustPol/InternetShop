c44958faeb9564934468bf368ddb7909e71b78d7
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-03-02 16:04:02 +0100

    [Created] Edit ServiceType
    Scaffolded Edit ServiceType

---
+++ LearningAspNetCore_InternetShop/Pages/ServiceTypes/Edit.cshtml.cs
@@ -0,0 +1,71 @@
+﻿using LearningAspNetCore_InternetShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace LearningAspNetCore_InternetShop.Pages.ServiceTypes
+{
+    public class EditModel : PageModel
+    {
+        private readonly Data.ApplicationDbContext _dbContext;
+
+        public EditModel(Data.ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _dbContext.ServiceTypes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync()
+        {
+            if (!ModelState.IsValid)
+            {
+                return Page();
+            }
+
+            _dbContext.Attach(ServiceType).State = EntityState.Modified;
+
+            try
+            {
+                await _dbContext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ServiceTypeExists(ServiceType.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return RedirectToPage("./Index");
+        }
+
+        private bool ServiceTypeExists(int id)
+        {
+            return _dbContext.ServiceTypes.Any(e => e.Id == id);
+        }
+    }
+}
