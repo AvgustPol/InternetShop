fe18b8ae3c8969f704035974eea2c1422a773a08
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-03-04 17:16:19 +0100

    [Added] Delete razor page

---
+++ LearningAspNetCore_InternetShop/Pages/ServiceTypes/Delete.cshtml.cs
@@ -0,0 +1,56 @@
+﻿using LearningAspNetCore_InternetShop.Data;
+using LearningAspNetCore_InternetShop.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using System.Threading.Tasks;
+
+namespace LearningAspNetCore_InternetShop.Pages.ServiceTypes
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public DeleteModel(ApplicationDbContext context)
+        {
+            _dbContext = context;
+        }
+
+        [BindProperty]
+        public ServiceType ServiceType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _dbContext.ServiceTypes.FirstOrDefaultAsync(m => m.Id == id);
+
+            if (ServiceType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            ServiceType = await _dbContext.ServiceTypes.FindAsync(id);
+
+            if (ServiceType != null)
+            {
+                _dbContext.ServiceTypes.Remove(ServiceType);
+                await _dbContext.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
