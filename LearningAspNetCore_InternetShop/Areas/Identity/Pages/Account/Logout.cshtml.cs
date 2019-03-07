55942baa8ed4830598bb199368275ce3477ef2ec
Anton Vlasiuk <vlasiuk.anton@gmail.com>
2019-03-07 13:03:12 +0100

    Fixed logout identity bug

--- LearningAspNetCore_InternetShop/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ LearningAspNetCore_InternetShop/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,20 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
 
 namespace LearningAspNetCore_InternetShop.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LogoutModel : PageModel
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
         private readonly ILogger<LogoutModel> _logger;
+        private readonly SignInManager<IdentityUser> _signInManager;
 
         public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
         {
@@ -36,7 +33,7 @@ namespace LearningAspNetCore_InternetShop.Areas.Identity.Pages.Account
             }
             else
             {
-                return Page();
+                return RedirectToPage("Logout");
             }
         }
     }
